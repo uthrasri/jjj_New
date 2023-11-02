bc72b78555f7ec68bce433481ec3937e8c4275ad
d.uthirapathy <d.uthirapathy@hcl.com>
2023-11-02 15:25:57 +0530

    Patch

---
+++ src/test/java/org/eclipse/jetty/server/CookieCutterTest.java
@@ -0,0 +1,357 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2020 Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.server;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.http.Cookie;
+
+import org.eclipse.jetty.http.CookieCompliance;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CookieCutterTest
+{
+    private Cookie[] parseCookieHeaders(CookieCompliance compliance, String... headers)
+    {
+        CookieCutter cutter = new CookieCutter(compliance);
+        for (String header : headers)
+        {
+            cutter.addCookieField(header);
+        }
+        return cutter.getCookies();
+    }
+
+    private void assertCookie(String prefix, Cookie cookie,
+                              String expectedName,
+                              String expectedValue,
+                              int expectedVersion,
+                              String expectedPath)
+    {
+        assertThat(prefix + ".name", cookie.getName(), is(expectedName));
+        assertThat(prefix + ".value", cookie.getValue(), is(expectedValue));
+        assertThat(prefix + ".version", cookie.getVersion(), is(expectedVersion));
+        assertThat(prefix + ".path", cookie.getPath(), is(expectedPath));
+    }
+
+    /**
+     * Example from RFC2109 and RFC2965
+     */
+    @Test
+    public void testRFCSingle()
+    {
+        String rawCookie = "$Version=\"1\"; Customer=\"WILE_E_COYOTE\"; $Path=\"/acme\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(1));
+        assertCookie("Cookies[0]", cookies[0], "Customer", "WILE_E_COYOTE", 1, "/acme");
+    }
+
+    /**
+     * Example from RFC2109 and RFC2965.
+     * <p>
+     * Lenient parsing, input has no spaces after ';' token.
+     * </p>
+     */
+    @Test
+    public void testRFCSingleLenientNoSpaces()
+    {
+        String rawCookie = "$Version=\"1\";Customer=\"WILE_E_COYOTE\";$Path=\"/acme\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(1));
+        assertCookie("Cookies[0]", cookies[0], "Customer", "WILE_E_COYOTE", 1, "/acme");
+    }
+
+    /**
+     * Example from RFC2109 and RFC2965
+     */
+    @Test
+    public void testRFCDouble()
+    {
+        String rawCookie = "$Version=\"1\"; " +
+            "Customer=\"WILE_E_COYOTE\"; $Path=\"/acme\"; " +
+            "Part_Number=\"Rocket_Launcher_0001\"; $Path=\"/acme\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "Customer", "WILE_E_COYOTE", 1, "/acme");
+        assertCookie("Cookies[1]", cookies[1], "Part_Number", "Rocket_Launcher_0001", 1, "/acme");
+    }
+
+    /**
+     * Example from RFC2109 and RFC2965
+     */
+    @Test
+    public void testRFCTriple()
+    {
+        String rawCookie = "$Version=\"1\"; " +
+            "Customer=\"WILE_E_COYOTE\"; $Path=\"/acme\"; " +
+            "Part_Number=\"Rocket_Launcher_0001\"; $Path=\"/acme\"; " +
+            "Shipping=\"FedEx\"; $Path=\"/acme\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(3));
+        assertCookie("Cookies[0]", cookies[0], "Customer", "WILE_E_COYOTE", 1, "/acme");
+        assertCookie("Cookies[1]", cookies[1], "Part_Number", "Rocket_Launcher_0001", 1, "/acme");
+        assertCookie("Cookies[2]", cookies[2], "Shipping", "FedEx", 1, "/acme");
+    }
+
+    /**
+     * Example from RFC2109 and RFC2965
+     */
+    @Test
+    public void testRFCPathExample()
+    {
+        String rawCookie = "$Version=\"1\"; " +
+            "Part_Number=\"Riding_Rocket_0023\"; $Path=\"/acme/ammo\"; " +
+            "Part_Number=\"Rocket_Launcher_0001\"; $Path=\"/acme\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "Part_Number", "Riding_Rocket_0023", 1, "/acme/ammo");
+        assertCookie("Cookies[1]", cookies[1], "Part_Number", "Rocket_Launcher_0001", 1, "/acme");
+    }
+
+    /**
+     * Example from RFC2109
+     */
+    @Test
+    public void testRFC2109CookieSpoofingExample()
+    {
+        String rawCookie = "$Version=\"1\"; " +
+            "session_id=\"1234\"; " +
+            "session_id=\"1111\"; $Domain=\".cracker.edu\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "session_id", "1234", 1, null);
+        assertCookie("Cookies[1]", cookies[1], "session_id", "1111", 1, null);
+    }
+
+    /**
+     * Example from RFC2965
+     */
+    @Test
+    public void testRFC2965CookieSpoofingExample()
+    {
+        String rawCookie = "$Version=\"1\"; session_id=\"1234\", " +
+            "$Version=\"1\"; session_id=\"1111\"; $Domain=\".cracker.edu\"";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC2965, rawCookie);
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "session_id", "1234", 1, null);
+        assertCookie("Cookies[1]", cookies[1], "session_id", "1111", 1, null);
+
+        cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+        assertThat("Cookies.length", cookies.length, is(1));
+        assertCookie("Cookies[0]", cookies[0], "session_id", "1111", 0, null);
+    }
+
+    /**
+     * Example from RFC6265
+     */
+    @Test
+    public void testRFC6265SidExample()
+    {
+        String rawCookie = "SID=31d4d96e407aad42";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(1));
+        assertCookie("Cookies[0]", cookies[0], "SID", "31d4d96e407aad42", 0, null);
+    }
+
+    /**
+     * Example from RFC6265
+     */
+    @Test
+    public void testRFC6265SidLangExample()
+    {
+        String rawCookie = "SID=31d4d96e407aad42; lang=en-US";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "SID", "31d4d96e407aad42", 0, null);
+        assertCookie("Cookies[1]", cookies[1], "lang", "en-US", 0, null);
+    }
+
+    /**
+     * Example from RFC6265.
+     * <p>
+     * Lenient parsing, input has no spaces after ';' token.
+     * </p>
+     */
+    @Test
+    public void testRFC6265SidLangExampleLenient()
+    {
+        String rawCookie = "SID=31d4d96e407aad42;lang=en-US";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "SID", "31d4d96e407aad42", 0, null);
+        assertCookie("Cookies[1]", cookies[1], "lang", "en-US", 0, null);
+    }
+
+    /**
+     * Basic name=value, following RFC6265 rules
+     */
+    @Test
+    public void testKeyValue()
+    {
+        String rawCookie = "key=value";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(1));
+        assertCookie("Cookies[0]", cookies[0], "key", "value", 0, null);
+    }
+
+    /**
+     * Basic name=value, following RFC6265 rules
+     */
+    @Test
+    public void testDollarName()
+    {
+        String rawCookie = "$key=value";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(0));
+    }
+
+    @Test
+    public void testMultipleCookies()
+    {
+        String rawCookie = "testcookie; server.id=abcd; server.detail=cfg";
+
+        // The first cookie "testcookie" should be ignored, per RFC6265, as it's missing the "=" sign.
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "server.id", "abcd", 0, null);
+        assertCookie("Cookies[1]", cookies[1], "server.detail", "cfg", 0, null);
+    }
+
+    @Test
+    public void testExcessiveSemicolons()
+    {
+        char[] excessive = new char[65535];
+        Arrays.fill(excessive, ';');
+        String rawCookie = "foo=bar; " + new String(excessive) + "; xyz=pdq";
+
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, rawCookie);
+
+        assertThat("Cookies.length", cookies.length, is(2));
+        assertCookie("Cookies[0]", cookies[0], "foo", "bar", 0, null);
+        assertCookie("Cookies[1]", cookies[1], "xyz", "pdq", 0, null);
+    }
+
+    @ParameterizedTest
+    @MethodSource("rfc6265Cookies")
+    public void testRFC6265CookieParsing(Param param)
+    {
+        Cookie[] cookies = parseCookieHeaders(CookieCompliance.RFC6265, param.input);
+
+        assertThat("Cookies.length (" + dump(cookies) + ")", cookies.length, is(param.expected.size()));
+        for (int i = 0; i < cookies.length; i++)
+        {
+            Cookie cookie = cookies[i];
+            assertThat("Cookies[" + i + "] (" + dump(cookies) + ")", cookie.getName() + "=" + cookie.getValue(), is(param.expected.get(i)));
+        }
+    }
+
+    public static List<Param> rfc6265Cookies()
+    {
+        return Arrays.asList(
+            new Param("A=1; B=2; C=3", "A=1", "B=2", "C=3"),
+            new Param("A=\"1\"; B=2; C=3", "A=1", "B=2", "C=3"),
+            new Param("A=\"1\"; B=\"2\"; C=\"3\"", "A=1", "B=2", "C=3"),
+            new Param("A=1; B=2; C=\"3", "A=1", "B=2"),
+            new Param("A=1 ; B=2; C=3", "A=1", "B=2", "C=3"),
+            new Param("A= 1; B=2; C=3", "A=1", "B=2", "C=3"),
+            new Param("A=\"1; B=2\"; C=3", "C=3"),
+            new Param("A=\"1; B=2; C=3"),
+            new Param("A=\"1 B=2\"; C=3", "A=1 B=2", "C=3"),
+            new Param("A=\"\"1; B=2; C=3", "B=2", "C=3"),
+            new Param("A=\"\" ; B=2; C=3", "A=", "B=2", "C=3"),
+            new Param("A=\"\"; B=2; C=3", "A=", "B=2", "C=3"),
+            new Param("A=1\"\"; B=2; C=3", "B=2", "C=3"),
+            new Param("A=1\"; B=2; C=3", "B=2", "C=3"),
+            new Param("A=1\"1; B=2; C=3", "B=2", "C=3"),
+            new Param("A=\" 1\"; B=2; C=3", "A= 1", "B=2", "C=3"),
+            new Param("A=\"1 \"; B=2; C=3", "A=1 ", "B=2", "C=3"),
+            new Param("A=\" 1 \"; B=2; C=3", "A= 1 ", "B=2", "C=3"),
+            new Param("A=\" 1 1 \"; B=2; C=3", "A= 1 1 ", "B=2", "C=3"),
+            new Param("A=1,; B=2; C=3", "B=2", "C=3"),
+            new Param("A=\"1,\"; B=2; C=3", "B=2", "C=3"),
+            new Param("A=\\1; B=2; C=3", "B=2", "C=3"),
+            new Param("A=\"\\1\"; B=2; C=3", "B=2", "C=3"),
+            new Param("A=1\u0007; B=2; C=3", "B=2", "C=3"),
+            new Param("A=\"1\u0007\"; B=2; C=3", "B=2", "C=3"),
+            new Param("€"),
+            new Param("@={}"),
+            new Param("$X=Y; N=V", "N=V"),
+            new Param("N=V; $X=Y", "N=V")
+        );
+    }
+
+    private static String dump(Cookie[] cookies)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (Cookie cookie : cookies)
+        {
+            sb.append("<").append(cookie.getName()).append(">=<").append(cookie.getValue()).append("> | ");
+        }
+        if (sb.length() > 0)
+            sb.delete(sb.length() - 2, sb.length() - 1);
+        return sb.toString();
+    }
+
+    private static class Param
+    {
+        private final String input;
+        private final List<String> expected;
+
+        public Param(String input, String... expected)
+        {
+            this.input = input;
+            this.expected = Arrays.asList(expected);
+        }
+
+        @Override
+        public String toString()
+        {
+            return input + " -> " + expected.toString();
+        }
+    }
+}
---
+++ src/test/java/org/eclipse/jetty/server/handler/InetAccessHandlerTest.java
@@ -0,0 +1,225 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2020 Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.server.handler;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.http.HttpTester;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InetAccessHandlerTest
+{
+    private static Server _server;
+    private static ServerConnector _connector1;
+    private static ServerConnector _connector2;
+    private static InetAccessHandler _handler;
+
+    @BeforeAll
+    public static void setUp() throws Exception
+    {
+        _server = new Server();
+        _connector1 = new ServerConnector(_server);
+        _connector1.setName("http_connector1");
+        _connector2 = new ServerConnector(_server);
+        _connector2.setName("http_connector2");
+        _server.setConnectors(new Connector[]
+            {_connector1, _connector2});
+
+        _handler = new InetAccessHandler();
+        _handler.setHandler(new AbstractHandler()
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+                throws IOException, ServletException
+            {
+                baseRequest.setHandled(true);
+                response.setStatus(HttpStatus.OK_200);
+            }
+        });
+        _server.setHandler(_handler);
+        _server.start();
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception
+    {
+        _server.stop();
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testHandler(String include, String exclude, String includeConnectors, String excludeConnectors, String code)
+        throws Exception
+    {
+        _handler.clear();
+        for (String inc : include.split(";", -1))
+        {
+            if (inc.length() > 0)
+            {
+                _handler.include(inc);
+            }
+        }
+        for (String exc : exclude.split(";", -1))
+        {
+            if (exc.length() > 0)
+            {
+                _handler.exclude(exc);
+            }
+        }
+        for (String inc : includeConnectors.split(";", -1))
+        {
+            if (inc.length() > 0)
+            {
+                _handler.includeConnector(inc);
+            }
+        }
+        for (String exc : excludeConnectors.split(";", -1))
+        {
+            if (exc.length() > 0)
+            {
+                _handler.excludeConnector(exc);
+            }
+        }
+
+        List<String> codePerConnector = new ArrayList<>();
+        for (String nextCode : code.split(";", -1))
+        {
+            if (nextCode.length() > 0)
+            {
+                codePerConnector.add(nextCode);
+            }
+        }
+
+        testConnector(_connector1.getLocalPort(), include, exclude, includeConnectors, excludeConnectors, codePerConnector.get(0));
+        testConnector(_connector2.getLocalPort(), include, exclude, includeConnectors, excludeConnectors, codePerConnector.get(1));
+    }
+
+    private void testConnector(int port, String include, String exclude, String includeConnectors, String excludeConnectors, String code) throws IOException
+    {
+        try (Socket socket = new Socket("127.0.0.1", port);)
+        {
+            socket.setSoTimeout(5000);
+
+            HttpTester.Request request = HttpTester.newRequest();
+            request.setMethod("GET");
+            request.setURI("/path");
+            request.setHeader("Host", "127.0.0.1");
+            request.setVersion(HttpVersion.HTTP_1_0);
+
+            ByteBuffer output = request.generate();
+            socket.getOutputStream().write(output.array(), output.arrayOffset() + output.position(), output.remaining());
+            HttpTester.Input input = HttpTester.from(socket.getInputStream());
+            HttpTester.Response response = HttpTester.parseResponse(input);
+            Object[] params = new Object[]
+                {
+                    "Request WBHUC", include, exclude, includeConnectors, excludeConnectors, code, "Response",
+                    response.getStatus()
+                };
+            assertEquals(Integer.parseInt(code), response.getStatus(), Arrays.deepToString(params));
+        }
+    }
+
+    /**
+     * Data for this test.
+     *
+     * @return Format of data: include;exclude;includeConnectors;excludeConnectors;assertionStatusCodePerConnector
+     */
+    public static Stream<Arguments> data()
+    {
+        Object[][] data = new Object[][]
+            {
+                // Empty lists 1
+                {"", "", "", "", "200;200"},
+
+                // test simple filters
+                {"127.0.0.1", "", "", "", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "", "", "200;200"},
+                {"192.0.0.1", "", "", "", "403;403"},
+                {"192.0.0.1-192.0.0.254", "", "", "", "403;403"},
+
+                // test includeConnector
+                {"127.0.0.1", "", "http_connector1", "", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "http_connector1", "", "200;200"},
+                {"192.0.0.1", "", "http_connector1", "", "403;200"},
+                {"192.0.0.1-192.0.0.254", "", "http_connector1", "", "403;200"},
+                {"192.0.0.1", "", "http_connector2", "", "200;403"},
+                {"192.0.0.1-192.0.0.254", "", "http_connector2", "", "200;403"},
+
+                // test includeConnector names where none of them match
+                {"127.0.0.1", "", "nothttp", "", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "nothttp", "", "200;200"},
+                {"192.0.0.1", "", "nothttp", "", "200;200"},
+                {"192.0.0.1-192.0.0.254", "", "nothttp", "", "200;200"},
+
+                // text excludeConnector
+                {"127.0.0.1", "", "", "http_connector1", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "", "http_connector1", "200;200"},
+                {"192.0.0.1", "", "", "http_connector1", "200;403"},
+                {"192.0.0.1-192.0.0.254", "", "", "http_connector1", "200;403"},
+                {"192.0.0.1", "", "", "http_connector2", "403;200"},
+                {"192.0.0.1-192.0.0.254", "", "", "http_connector2", "403;200"},
+
+                // test excludeConnector where none of them match.
+                {"127.0.0.1", "", "", "nothttp", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "", "nothttp", "200;200"},
+                {"192.0.0.1", "", "", "nothttp", "403;403"},
+                {"192.0.0.1-192.0.0.254", "", "", "nothttp", "403;403"},
+
+                // both connectors are excluded
+                {"127.0.0.1", "", "", "http_connector1;http_connector2", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "", "http_connector1;http_connector2", "200;200"},
+                {"192.0.0.1", "", "", "http_connector1;http_connector2", "200;200"},
+                {"192.0.0.1-192.0.0.254", "", "", "http_connector1;http_connector2", "200;200"},
+
+                // both connectors are included
+                {"127.0.0.1", "", "http_connector1;http_connector2", "", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "http_connector1;http_connector2", "", "200;200"},
+                {"192.0.0.1", "", "http_connector1;http_connector2", "", "403;403"},
+                {"192.0.0.1-192.0.0.254", "", "http_connector1;http_connector2", "", "403;403"},
+
+                // exclude takes precedence over include
+                {"127.0.0.1", "", "http_connector1;http_connector2", "http_connector1;http_connector2", "200;200"},
+                {"127.0.0.1-127.0.0.254", "", "http_connector1;http_connector2", "http_connector1;http_connector2", "200;200"},
+                {"192.0.0.1", "", "http_connector1;http_connector2", "http_connector1;http_connector2", "200;200"},
+                {"192.0.0.1-192.0.0.254", "", "http_connector1;http_connector2", "http_connector1;http_connector2", "200;200"}
+            };
+        return Arrays.asList(data).stream().map(Arguments::of);
+    }
+}
